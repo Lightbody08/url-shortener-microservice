35b05c18cf1141836b30f159c3c064aef9e4fbe0
Ryan Lightbody <Ryan Lightbody>
2016-07-25 19:13:14 -0400

    added mongodb

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var mongodb = require('mongodb');
 
-app.get('/', function (req, res) {
-	res.send('Hello World!');
+var MongoClient = mongodb.MongoClient;
+
+var url = process.env.MONGOLAB_URI;
+
+app.get('/new/:url(*)', function (req, res) {
+	res.send(req.params.url);
+	console.log(req.params.url);
+});
+
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log("Unable to connect to server:", err);
+  } else {
+    console.log("Connected to server!")
+  };
 });
 
 app.listen(process.env.PORT || 80, function () {
-	if(process.env.PORT){
+	if(process.env.PORT) {
 		console.log('Example app listening on port' + process.env.PORT + '!');
 	}
-	if(80){
+	if(80) {
 		console.log('Example app listening on port 80!');
 	}
 	
